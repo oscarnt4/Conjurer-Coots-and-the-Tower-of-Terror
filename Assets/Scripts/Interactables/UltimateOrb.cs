9d773a0487e2c128de5cbf24eb6e1a89dd532af4
oscarnt4 <ojep@live.co.uk>
2023-02-21 15:10:14 +0000

    Added ultimate ability
    added ultimate ability which destroys every enemy touching the tower

---
+++ Assets/Scripts/Interactables/UltimateOrb.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UltimateOrb : Interactable
+{
+    [SerializeField] float attackChargeTime = 2f;
+    [SerializeField] PlayerMana playerMana;
+    [SerializeField] int manaCost = 50;
+    [SerializeField] PlayerUI playerUI;
+    [SerializeField] GameObject ultimateProjectile;
+    [SerializeField] Transform projectileSpawnLoaction;
+
+    private Material orbMaterial;
+    private Color originalColour;
+    private bool isCharging = false;
+    private float currentChargeTime = 0f;
+
+    void Awake()
+    {
+        orbMaterial = GetComponent<Renderer>().material;
+    }
+    void Start()
+    {
+        originalColour = orbMaterial.color;
+    }
+
+    void Update()
+    {
+        if (isCharging)
+        {
+            currentChargeTime += Time.deltaTime;
+        }
+        if (currentChargeTime >= attackChargeTime)
+        {
+            orbMaterial.color = Color.red;
+        }
+    }
+
+    protected override void Interact()
+    {
+        if (!isCharging)
+        {
+            if (playerMana.UpdateMana(-manaCost))
+            {
+                playerMana.UpdateMana(manaCost);
+                orbMaterial.EnableKeyword("_EMISSION");
+                if (!isCharging)
+                {
+                    isCharging = true;
+                    currentChargeTime = 0f;
+                }
+            }
+            else
+            {
+                playerUI.UpdateText("NOT ENOUGH MANA");
+            }
+        }
+        if (isCharging && currentChargeTime < attackChargeTime)
+        {
+            playerUI.UpdateText("Charging...");
+        }
+    }
+
+    protected override void ReleaseInteract()
+    {
+        if (isCharging)
+        {
+            if (currentChargeTime >= attackChargeTime)
+            {
+                Projectile projectile = Instantiate(ultimateProjectile, projectileSpawnLoaction).GetComponent<Projectile>();
+                projectile.ReleaseProjectile();
+                playerMana.UpdateMana(-manaCost);
+            }
+        }
+
+        currentChargeTime = 0f;
+        orbMaterial.DisableKeyword("_EMISSION");
+        orbMaterial.color = originalColour;
+        isCharging = false;
+    }
+}
